2f3409a26872e82ec811b02116b37aba88d7f3ee
Anna <azeleranskaya@gmail.com>
2024-09-17 22:50:48 +0300

    changes to be pushed to GitHub

--- file1.py
+++ file1.py
@@ -1,3 +1,8 @@
-print("file21")
-print("file1")
-print("file123")
+from typing import List
+
+
+def get_same_numbers(list_1: List[int], list_2: List[int]) -> List[int]:
+    return [i for i in list_1 if i in list_2]
+
+
+print(get_same_numbers([1, 2, 3, 4], [3, 4, 5, 6]))
--- file2.py
+++ file2.py
@@ -1,3 +1,21 @@
-print("file2")
-print("file2")
-print("file2")
+def circle_area(r: int) -> float:
+    """Подсчитывает площадь круга от заданного радиуса."""
+    pi = 3.14
+    circle_area = pi * r**2
+    return circle_area
+
+
+def format_description(r, area):
+    """Определяет формат вывода сообщения о радиусе и площади круга."""
+    return "Radius is " + str(r) + "; area is " + str(round(area, 2))
+
+
+def get_info(r: int):
+    """Ввывод информации о площади круга по радиусу по заданному формату."""
+    area = circle_area(r)
+    description = format_description(r, area)
+    print(description)
+
+
+radius = int(input("Enter circle radius (int): "))
+get_info(radius)
